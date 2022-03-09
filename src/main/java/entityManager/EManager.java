b9cd3c2bfcf66a0d333e90614fb9b49155cd2b65
Francisco Troncoso <frantron01@gmail.com>
2022-03-09 11:53:16 -0300

    entity manager

---
+++ src/main/java/entityManager/EManager.java
@@ -0,0 +1,12 @@
+package entityManager;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class EManager {
+    public static EntityManager getEntityManager() {
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("persistence");
+        return factory.createEntityManager();
+    }
+}
